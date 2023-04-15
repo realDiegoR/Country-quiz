2acd7912647cd1ebfc7de767e215fc07b3836475
realdiegor <diegoreyes150103@gmail.com>
2023-04-14 23:20:53 -0400

    add tailwind, typescript and parcel

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/index.html", "./src/**/*.{js,ts}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				background: "url('/src/assets/img/background.png')",
+			},
+			fontFamily: {
+				poppins: "Poppins, sans-serif",
+			},
+		},
+	},
+	plugins: [],
+};
